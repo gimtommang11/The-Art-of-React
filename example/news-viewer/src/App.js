39a5b577845290f955e44a7eed705f34ac51c4fa
SuHaLee <gimtommang88@gmail.com>
2019-11-09 18:47:33 +0900

    fix 401 error
    401 에러 고침, 링크 다시 복사해오니 잘됬음

--- example/news-viewer/src/App.js
+++ example/news-viewer/src/App.js
@@ -1,17 +1,20 @@
-import React,{useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList/NewsList';
 import Categories from './components/Categories/Categories';
 
-const App =()=>{
-  const [category, setCategory]=useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);    // 카테고리 값 업데이트 해주는 함수
-  return(
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => {
+    setCategory(category);
+    console.log(category);
+  }, []); // 카테고리 값 업데이트 해주는 함수
+  return (
     <>
-      <Categories category={category} onSelect={onSelect}/>
-      <NewsList category={category}/>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
-}
+};
 
 export default App;
