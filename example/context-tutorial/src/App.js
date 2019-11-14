fcc67ba5de9f8a91a3ea6840293dc0cb1cb2cf56
SuHaLee <gimtommang88@gmail.com>
2019-11-14 17:04:33 +0900

    update app.js

--- example/context-tutorial/src/App.js
+++ example/context-tutorial/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    //provider 사용해서 context value 변경. createContext 함수 사용할때는 넣어준 기본값은 provider 사용하지 않을 때만 사용됨
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
