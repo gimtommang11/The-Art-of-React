eb9138be8d6fcbdf507cfeced19afb58a00136d8
SuHaLee <gimtommang88@gmail.com>
2019-11-14 11:30:37 +0900

    Create context-tutorial
    context-tutorial cra로 생성, context 생성

---
+++ example/context-tutorial/src/components/ColorBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ColorContext from "../contexts/color";
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {" "}
+      {/*Consumer component 이용해 색상(상태) 조회*/}
+      {(
+        value //Function as a child. 컴포넌트 childeren 있는 자리에 일반 jsx or 문자열이 아닌 함수 전달
+      ) => (
+        <div
+          style={{
+            width: "64px",
+            height: "64px",
+            background: value.color
+          }}
+        />
+      )}
+    </ColorContext.Consumer>
+  );
+};
+
+export default ColorBox;
