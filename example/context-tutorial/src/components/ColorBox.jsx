8954232751309954bafaf7be82f9d51cf00fc172
SuHaLee <gimtommang88@gmail.com>
2019-11-14 20:16:02 +0900

    Update Colorbox with useContext
    useContext 훅을 이용해서 바꿈

--- example/context-tutorial/src/components/ColorBox.jsx
+++ example/context-tutorial/src/components/ColorBox.jsx
@@ -1,32 +1,26 @@
-import React from "react";
+import React, { useContext } from "react"; //useContext hooks
 import ColorContext, { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext); //creatContext함수를 그대로 파라미터로 넘겨주어야 함
   return (
     //*Consumer component 이용해 색상(상태) 조회*
-    <ColorConsumer>
-      {(
-        //Function as a child. 컴포넌트 childeren 있는 자리에 일반 jsx or 문자열이 아닌 함수 전달
-        { state } //비구조화 할당을 이용해, value 조회 생략
-      ) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor
+        }}
+      />
+    </>
   );
 };
 
