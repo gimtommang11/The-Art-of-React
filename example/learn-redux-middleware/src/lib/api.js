5570e8a497d514e58b2b4b6a08f3b7656d1a4eb3
SuHaLee <gimtommang88@gmail.com>
2019-12-01 02:43:24 +0900

    create Api, redux
    api호출하는 함수 생성,/ 통신하는 엑션, 엑션생성자, 리듀서 생성

---
+++ example/learn-redux-middleware/src/lib/api.js
@@ -0,0 +1,10 @@
+// api를 호출하는 함수 따로 생성, 가독성과 유지보수할때 좋음. 
+import axios from 'axios';
+
+export const getPost = id => { //다른 파일에서 이를 import해서 사용함
+  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+}
+
+export const getUsers = id => {
+  axios.get(`https://jsonplaceholder.typicode.com/users`);
+}
---
+++ example/learn-redux-middleware/src/models/sample.js
@@ -0,0 +1,115 @@
+import {
+  handleActions
+} from 'redux-actions';
+import * as api from '../lib/api';
+
+// 엑션 타입 생성
+// 한 요청당 세 개를 만들어야 함.
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+//thunk 함수 생성
+//thunk함수 내부에서는 시작할때, 성공했을때, 실패했을때 다른 액션을 디스패치함
+
+export const getPost = id => async dispatch => {
+  dispatch({
+    type: GET_POST
+  }); // 요청 시작 알림
+  try {
+    const response = await api.getPost(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data
+    }); //요청 성공
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true
+    }); //에러 발생
+    throw e;
+  }
+};
+
+export const getUsers = () => async dispatch => {
+  dispatch({
+    type: GET_USERS
+  }); // 요청을 시작한 것을 알림
+  try {
+    const response = await api.getUsers();
+    dispatch({
+      type: GET_USERS_SUCCESS,
+      payload: response.data
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true
+    })
+  }
+}
+
+//초기 상태 관리
+//요청의 로딩 중 상태는 loading이라는 객체에서 관리함
+
+const initialState = {
+  loading: {
+    GET_POST: false,
+    GET_USERS: false
+  },
+  post: null,
+  users: null
+};
+
+const sample = handleActions({
+  [GET_POST]: state => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_POST: true //요청 시작
+    }
+  }),
+  [GET_POST_SUCCESS]: (state, action) => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_POST: false
+    }
+  }),
+  [GET_POST_FAILURE]: (state, action) => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_POST: false
+    }
+  }),
+  [GET_USERS]: (state) => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_USERS: true
+    }
+  }),
+  [GET_USERS_SUCCESS]: (state, action) => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_USERS: false
+    }
+  }),
+  [GET_POST_SUCCESS]: (state, action) => ({
+    ...state,
+    loading: {
+      ...state.loading,
+      GET_USERS: false
+    }
+  }),
+  initialState
+})
