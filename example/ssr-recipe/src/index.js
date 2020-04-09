d39661703750bb1b97d1aed139a34eb64ddb4cd3
SuHaLee <gimtommang88@gmail.com>
2020-04-10 01:37:06 +0900

    create ssr-recipe project

---
+++ example/ssr-recipe/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
