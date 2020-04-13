423d7eceac034edafaf22c90b48c984216cd7542
SuHaLee <gimtommang88@gmail.com>
2020-04-14 00:23:24 +0900

    add publicUrlOrPath

--- example/ssr-recipe/config/paths.js
+++ example/ssr-recipe/config/paths.js
@@ -88,6 +88,7 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
+  publicUrlOrPath: getPublicUrlOrPath(resolveApp('package.json')),
   ssrIndexJs: resolveApp('src/index.server.js'), // 서버 사이드 렌더링 엔트리
   ssrBuild: resolveApp('dist') // 웹팩 처리 후 저장 경로
 };
